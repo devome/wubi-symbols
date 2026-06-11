72ce86a14d5fcfc2c6295fc49ae7c80c39077757
devome <evinedeng@hotmail.com>
2026-06-11 10:42:08 +0800

    update imewlconverter to 3.4.2

--- convert.sh
+++ convert.sh
@@ -5,7 +5,7 @@ wubi_vers=( wb86 wb98 wbnewage )
 
 ## 检测imewlconverter和dos2unix
 if ! type imewlconverter &>/dev/null; then
-    echo "请先安装 imewlconverter: https://github.com/studyzy/imewlconverter"
+    echo "请先安装 imewlconverter (>=3.4.1): https://github.com/studyzy/imewlconverter/releases"
     exit 1
 fi
 
@@ -19,9 +19,9 @@ awk -F '\t' '{print $3}' symbols/*.tsv | sed -E 's| |\n|g' | sort -u | grep -viP
 
 ## 转换为五笔键码
 mkdir -p output
-for ver in ${wubi_vers[@]}; do
-    imewlconverter -i:word dict-symbols.txt -o:$ver "output/$ver.txt"
-    dos2unix "output/$ver.txt"
+for wubi_ver in ${wubi_vers[@]}; do
+    imewlconverter --input-format word --output-format $wubi_ver --output "output/$wubi_ver.txt" dict-symbols.txt
+    dos2unix "output/$wubi_ver.txt"
 done
 
 ## 把symbol符号也生成对应的键码
@@ -29,15 +29,15 @@ while read line; do
     symbol=$(echo "$line" | awk -F '\t' '{print $2}')
     regex=$(echo "$line" | awk -F '\t' '{print $3}' | sed -E 's/ /\|/g')
 
-    for ver in ${wubi_vers[@]}; do
-        keysyms=( $(grep -P "^[a-z]{1,4} ($regex)$" "output/$ver.txt" | awk '{print $1}' | sort -u) )
+    for wubi_ver in ${wubi_vers[@]}; do
+        keysyms=( $(grep -P "^[a-z]{1,4} ($regex)$" "output/$wubi_ver.txt" | awk '{print $1}' | sort -u) )
         for keysym in ${keysyms[@]}; do
-            echo "$keysym $symbol" >> "output/$ver.txt"
+            echo "$keysym $symbol" >> "output/$wubi_ver.txt"
         done
 
         keycodes=( $(echo "$line" | awk -F '\t' '{print $3}' | grep -oP '[a-z]+') )
         for keycode in ${keycodes[@]}; do
-            echo "$keycode $symbol" >> "output/$ver.txt"
+            echo "$keycode $symbol" >> "output/$wubi_ver.txt"
         done
     done
 done <<< "$(cat symbols/*.tsv)"
